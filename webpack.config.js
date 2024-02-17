aa2531be1a069f4ab5b5bc734c0481a33d55e5d6
Emmanuel Barrios Perez <barriem4@hotmail.com>
2024-02-17 14:42:10 +0100

    otro cambio de carpeta

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path')
 
 module.exports = {
     mode: 'development',
-    entry: './src/index.js',
+    entry: '.docs/src/index.js',
     output: {
         path: path.resolve(__dirname, 'docs'),
         filename: 'bundle.js'
