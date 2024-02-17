ca0da522f96e509a9a213b099021cd51ffea4c8c
Emmanuel Barrios Perez <barriem4@hotmail.com>
2024-02-17 15:14:34 +0100

    se agrego jquery, creo

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,8 @@ const path = require('path')
 
 module.exports = {
     mode: 'development',
-    entry: '.docs/src/index.js',
+    // entry: '.docs/src/index.js',
+    entry: ['./docs/src/index.js', './docs/src/jquery.js'],
     output: {
         path: path.resolve(__dirname, 'docs'),
         filename: 'bundle.js'
